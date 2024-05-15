508834367213d79c3aa46270ce3dc524eb320d74
WilliamZeng <itswilliamzeng@gmail.com>
2024-05-15 15:55:54 -0400

    short anwer

--- src/modify.js
+++ src/modify.js
@@ -1,26 +1,20 @@
 // These are your event handlers
-const clickCounterHandler = () => {
-};
+const clickCounterHandler = () => {};
 
-const handleKeydown = () => {
-};
+const handleKeydown = () => {};
 
 // We've started this one for you
 const handleDelegation = (event) => {
-  const resultSpan = document.querySelector('#delegation-result');
-  resultSpan.textContent = event.target.textContent;
+	const resultSpan = document.querySelector('#delegation-result');
+	resultSpan.textContent = event.target.textContent;
 };
 
-const addNewRandomNumber = () => {
-};
-
+const addNewRandomNumber = () => {};
 
 // Select the elements and attach your event handlers inside main
 const main = () => {
-  const delegationContainer = document.querySelector('#delegation');
-  delegationContainer.addEventListener('click', handleDelegation);
-
-  
+	const delegationContainer = document.querySelector('#delegation');
+	delegationContainer.addEventListener('click', handleDelegation);
 };
 
 main();
