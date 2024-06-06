708cde7ce3db2fa68f6e99d1e32de4604e7139e7
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-06 13:32:06 -0400

    feedback

--- src/modify.js
+++ src/modify.js
@@ -1,4 +1,5 @@
 // These are your event handlers
+/** FEEDBACK: Great job getting all test cases to pass, in the future, clean up your code by removing any commented out code! */
 const clickCounterHandler = (event) => {
 	console.log(event.target);
 	const button = event.target;
